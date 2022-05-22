584da7ef2ba7bacf23194b96626858b539db917e
Luan-S4ntana <95152434+Luan-S4ntana@users.noreply.github.com>
2022-05-22 00:49:53 -0300

    buttons, labels, size

--- Cadastro Pizzaria/src/View/Menu.java
+++ Cadastro Pizzaria/src/View/Menu.java
@@ -2,21 +2,34 @@ package View;
 
 import javax.swing.*;
 
-public class Menu {
+public class Menu extends JFrame {
     public Menu() {
-        JFrame frame = new JFrame ("Lista de Produtos");
-        JTextField label = new JTextField("Interface gráfica");
-        JTextField field1 = new JTextField("Nome Fantasia");
-        JTextField field2 = new JTextField("Nome do Representante");
-        JTextField field3 = new JTextField("Tipo de empresa");
-        JTextField field4 = new JTextField("Cnpj");
-        JTextField field5 = new JTextField("Informe o produto a ser cadastrado");
+        JFrame frame = new JFrame();
+        JLabel label = new JLabel("Nome");
+        //JLabel label1 = new JLabel("telefone");
+        //JLabel label2 = new JLabel("cpf");
+        //JLabel label3 = new JLabel("rua");
+        //JLabel label4 = new JLabel("Numero");
+        //JLabel label5 = new JLabel("Bairro");
+        //JLabel label6 = new JLabel("Estado");
+        //JLabel label7 = new JLabel("Cidade");
         frame.getContentPane().add(label);
-  
+        //frame.getContentPane().add(label1);
+        //frame.getContentPane().add(label2);
+        //frame.getContentPane().add(label3);
+        //frame.getContentPane().add(label4);
+        //frame.getContentPane().add(label5);
+        //frame.getContentPane().add(label6);
+        //frame.getContentPane().add(label7);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-        frame.setSize(640, 480);
+        frame.setSize(800, 600);
+        //JPanel painel = new JPanel();
+        JButton b = new JButton("Cadastrar Cliente");
+        JButton b1 = new JButton("Cadastrar Produto");
+        JButton b2 = new JButton("Procurar cliente");
+        JButton b3 = new JButton("Enviar cupom de desconto");
     }
 
 }
